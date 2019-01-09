5f4e7ae1235b8c28441b612d47fe0b4752363095
Kuba <kuba.pod@gmail.com>
2019-01-09 16:19:30 +0100

    dev: new documentation init

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,18 +1,19 @@
-(* Paclet Info File *)
-
-(* created 2018/06/19*)
-
 Paclet[
-    Name -> "VQM",
-    Version -> "0.0.1",
-    MathematicaVersion -> "6+",
-    Description -> "New Documentation for VQM",
-    Extensions -> 
-        {
-            {"Documentation", Resources -> 
-                {"Guides/VQM-Guide"}
-            , Language -> "English"}
-        }
-]
+  Name -> "VQM",
+  Version -> "0.0.1",
+  Description -> "New Documentation for VQM",
+  MathematicaVersion -> "6+",
+  Extensions -> {
 
+    	{
 
+     		"Documentation",
+
+     		"Resources" -> {"Guides/VQM-Guide"},
+
+     		"Language" -> "English"
+
+     	}
+
+    }
+ ]
